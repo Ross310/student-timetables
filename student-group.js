b349211aa855e61483951e7803cbe06f86b81c10
Ross310 <rosi02@blackburn.ac.uk>
2025-10-24 10:10:46 +0100

    Add files via upload

---
+++ student-group.js
@@ -0,0 +1,109 @@
+
+// CSV Content for Student ID to Course Group mapping (Groups for TT yr1.csv)
+export const idMapCSVContent = `id,name,course
+20368757,"Afzal, Miss Sabah ",S118ED-Y1-G1
+20378618,"Khan, Miss Zulaykha Noor",S118ED-Y1-G1
+20377797,"Muhammad, Mr Talha Khan",S118ED-Y1-G1
+20381185,"Nawaz, Mr Qasim Qasim",S118ED-Y1-G1
+20380736,"Razaq, Mr Muneeb Ashfaq",S118ED-Y1-G1
+20378471,"Saju, Mr Simon ",S118ED-Y1-G1
+20379858,"Solkar, Mr Yousuf Ibrahim",S118ED-Y1-G1
+20385511,"Willis, Mr Wade King",S118ED-Y1-G1
+20380791,"Dar, Mr Aaryan Aaryan",S118ED-Y1-G1
+20379159,"Farag, Mr Ahmed Abdelrahman",S118ED-Y1-G1
+20374891,"Hickson, Mx Aidan ",S118ED-Y1-G1
+20380117,"Hussain, Mr Mohammed-Murtaza ",S118ED-Y1-G1
+20379909,"Ali Kanwal, Miss Sheeza ",S118ED-Y1-G1
+20381824,"Adams, Miss Charlotte Elizabeth",S118ED-Y1-G1
+20378862,"Munshi, Fayaz",S118ED-Y1-G1
+20379560,"Kouser, Mr Ali Mehmood",S118ED-Y1-G1
+20364571,"Khansia, Mr Layth Ahmed",S118ED-Y1-G1
+20379937,"Akhtar, Mr Aayan Kaser",S118ED-Y1-G1
+20360670,"Iqbal, Miss Shabana Kiran",S118ED-Y1-G2
+20379362,"Choudhry, Waniya ",S118ED-Y1-G2
+20368611,"Aslam, Mr Zidhaan ",S118ED-Y1-G2
+20381228,"Butt, Mr Sami ",S118ED-Y1-G2
+20378014,"Ghisa, Miss Aiman ",S118ED-Y1-G2
+20359335,"Hesketh, Miss Leona Nicole",S118ED-Y1-G2
+20377761,"Ibrahim, Mr Zain ",S118ED-Y1-G2
+20381477,"John, Miss Wisdom Amarachukwu",S118ED-Y1-G2
+20382471,"Oluwasegun, Miss Divine ",S118ED-Y1-G2
+20379157,"Patel, Miss Humayra ",S118ED-Y1-G2
+20384638,"Patel, Mr Hamza ",S118ED-Y1-G2
+20380340,"Shaheen, Miss Tayba ",S118ED-Y1-G2
+20379456,"Tai, Mr Uzair ",S118ED-Y1-G2
+20378753,Aiysha Kousar,S118ED-Y1-G2
+20378464,Bibi,S118ED-Y1-G2
+20378430,Bibi,S118ED-Y1-G2
+20380322,"Adam, Mr Humzah ",S118ED-Y1-G3
+20379316,"Al-Doseri, Miss Saja Al'issa",S118ED-Y1-G3
+20370815,"Alli, Miss Husnaa Sajid",S118ED-Y1-G3
+20379395,"Hassan, Miss Safah ",S118ED-Y1-G3
+20386204,"Iftikhar, Mr Ahsan ",S118ED-Y1-G3
+20360019,"Ilunga, Mr Daniel Kabanga",S118ED-Y1-G3
+20380864,"Ilyas, Miss Humairah ",S118ED-Y1-G3
+20378778,"Khan, Miss Maryam ",S118ED-Y1-G3
+20381032,"Malik, Miss Imaan Jan",S118ED-Y1-G3
+20385563,"Malik, Mr Faizaan ",S118ED-Y1-G3
+20378501,"Mohammed, Miss Maariah ",S118ED-Y1-G3
+20379445,"Shah, Miss Fiza Haroon",S118ED-Y1-G3
+20381125,"Vachhiyat, Mr Muhammad ",S118ED-Y1-G3
+20384188,"Hussain, Fatima",S118ED-Y1-G3
+20385488,"Hussain, Maryam",S118ED-Y1-G3
+20371539,"Hussain, Ms Sana ",S118ED-Y1-G3
+20378656,"Iqbal, Miss Sara ",S118ED-Y1-G3
+20374614,"Rahman, Mr Shaafi ",S118ED-Y1-G3
+20378911,"Begum, Miss Meena ",S118ED-Y1-G4
+20378686,"Jamil, Miss Alishba ",S118ED-Y1-G4
+20368896,"Adam, Miss Munira ",S118ED-Y1-G4
+20380214,"Ahmed, Miss Anam Kausar",S118ED-Y1-G4
+20369179,"Alsaedi, Miss Teeba Mohammad",S118ED-Y1-G4
+20366523,"Bhatti, Miss Halimah Kibriyah",S118ED-Y1-G4
+20385556,"Chaudhry, Miss Zara ",S118ED-Y1-G4
+20377644,"Fulat, Miss Tayyabah ",S118ED-Y1-G4
+20369369,"Iqbal, Miss Nayab ",S118ED-Y1-G4
+20369331,"Moazam, Miss Hifsa ",S118ED-Y1-G4
+20381551,"Nguyen, Miss Maryam ",S118ED-Y1-G4
+20378173,"Riaz, Miss Laiba Afzal",S118ED-Y1-G4
+20380266,"Saddat, Miss Perka ",S118ED-Y1-G4
+20378705,"Yousaf, Mr Muhammad Abdullah",S118ED-Y1-G4
+20357562,Khan,S118ED-Y1-G4
+20379896,"Sardar, Mr Issa ",S118ED-Y1-G4
+20380681,"Khan, Mr Mohammed Sultan",S118ED-Y1-G4
+20385637,"Ahmed, Mr Hamza ",S118ED-Y1-G4
+20384550,"Chaudhry, Mr Owais Owais Shahzad",S118ED-Y1-G5
+20381707,"Hamza, Mr Ali ",S118ED-Y1-G5
+20381018,"Akhtar, Mr Ali-Raza ",S118ED-Y1-G5
+20380744,"Ali, Miss Haleema ",S118ED-Y1-G5
+20380154,"Balesaria, Mr Sahl ",S118ED-Y1-G5
+20372340,"Freeman, Miss Sophie Riayah",S118ED-Y1-G5
+20370037,"Imtiaz, Miss Rubab ",S118ED-Y1-G5
+20378499,"Lambat, Miss Aamina Hassan",S118ED-Y1-G5
+20379819,"Malagocki, Mr Igor ",S118ED-Y1-G5
+20377975,"Malik, Miss Safira ",S118ED-Y1-G5
+20376017,"Ravat, Mr Sufyaan ",S118ED-Y1-G5
+20385481,"Rizwan, Miss Aneesa ",S118ED-Y1-G5
+20382978,"Sheikh, Miss Hajira ",S118ED-Y1-G5
+20370790,"Shoby, Miss Helen Angel ",S118ED-Y1-G5
+20380391,"Singh, Mr Jay Jay",S118ED-Y1-G5
+20378491,"Syed, Miss Javeriya Fatima",S118ED-Y1-G5
+20380684,"West-Qasam, Miss Hanna Bridget",S118ED-Y1-G5
+20379059,"Patel, Mr Ayaan ",S118ED-Y1-G6
+20381782,"Rehman, Mr Qasim ",S118ED-Y1-G6
+20378360,"Ahmed, Miss Aqsa ",S118ED-Y1-G6
+20381544,"Alennezi, Miss Fatima ",S118ED-Y1-G6
+20359110,"Azram, Miss Komal ",S118ED-Y1-G6
+20369772,"Badat, Mr Muhammad ",S118ED-Y1-G6
+20368395,"Begum, Miss Mariya ",S118ED-Y1-G6
+20385443,"Beha, Mr Abdel Rahman ",S118ED-Y1-G6
+20382278,"Billington, Mr Liam Jean",S118ED-Y1-G6
+20384712,"Iqbal, Mr Hamza Muhammad",S118ED-Y1-G6
+20365988,"Patel, Miss Raeesah Juned",S118ED-Y1-G6
+20385329,"Smith, Miss Hayden ",S118ED-Y1-G6
+20370847,"Suleman, Mr Mohammed Bilal",S118ED-Y1-G6
+20378500,"Ugradar, Miss Fatima ",S118ED-Y1-G6
+20378607,"Yasif, Miss Ameera ",S118ED-Y1-G6
+20378447,"Majeed, Miss Juwariyah ",S118ED-Y1-G6
+20347528,"Patel, Mr Mohammed Zain ",S118ED-Y1-G6
+20369989,"Bhatti, Mr Mubarak Asad Ali",S118ED-Y1-G6
+`;
